e70164779930272643448e05728075ef02602f2a
glowcouch <42631203+glowcouch@users.noreply.github.com>
2022-10-18 20:44:38 +1300

    Wrote some code that loads data from Data.csv

---
+++ Answer.cs
@@ -0,0 +1,11 @@
+namespace StatisticsProject
+{
+    class Answer
+    {
+        public string Name {get; set;}
+        public int LanEver {get; set;}
+        public int LanRegular {get; set;}
+        public int GamesRegular {get; set;}
+        public int ScreenTime {get; set;}
+    }
+}
---
+++ Main.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using CsvHelper;
+
+namespace StatisticsProject
+{
+    class Graph
+    {
+        static void Main(string[] args)
+        {
+            //Get data from Data.csv
+            StreamReader reader = new StreamReader("Data.csv");
+            CsvReader csv = new CsvReader(reader, System.Globalization.CultureInfo.InvariantCulture);
+            IEnumerable<Answer> answers = csv.GetRecords<Answer>();
+        }
+    }
+}
