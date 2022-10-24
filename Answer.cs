e923f04034395ce791af227f8c9b6e4a1d01d7cd
Finn Montgomery-Klien <42631203+glowcouch@users.noreply.github.com>
2022-10-24 21:09:36 +0000

    Added the final data

--- Answer.cs
+++ Answer.cs
@@ -3,8 +3,8 @@ namespace StatisticsProject
     class Answer
     {
         public string Name {get; set;}
-        public int LanEver {get; set;}
-        public int LanRegular {get; set;}
+        public string FavTech {get; set;}
+        public float SportHours {get; set;}
         public float BooksWeek {get; set;}
         public float ScreenTime {get; set;}
         public int WPM {get; set;}
